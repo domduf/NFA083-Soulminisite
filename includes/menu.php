1bc7a4c4d51de49a48f3ca98614c3e028df9a8cc
domduf <minique.duf@gmail.com>
2016-05-08 15:53:46 +0200

    correction du css: menu en haut du bloc central

--- includes/menu.php
+++ includes/menu.php
@@ -1,27 +1,36 @@
 <!-- ici se trouve le menu, que l'on retrouve sur chaque page du site-->
+<div id = posMenu>
 
 <nav id = menu>
-<ul>
-
-<li> <a href="./index.php">Accueil</a></li>
-
-<li> <a href="./revuePresse.php">Revue de presse</a>
 	<ul>
-		<li><a href="./avant2010.php">avant 2010</a></li>
-		<li><a href="./depuis2010.php">depuis 2010</a></li>
+
+		<li> <a href="./index.php">Accueil</a></li>
+
+		<li> <a href="./revuePresse.php">Revue de presse</a>
+			<ul>
+				<li><a href="./avant2010.php">avant 2010</a></li>
+				<li><a href="./depuis2010.php">depuis 2010</a></li>
+
+			</ul>
+		</li>
+
+		<li> <a href="./sons.php">Sons</a></li>
+
+		<li> <a href="./agenda.php">Agenda.</a></li>
+
+		<li><a href="./lesMusicos.php">Les musiciens.</a></li>
+
+		<li><a href="./formulaireRenseignement.php">Formulaire contact.</a></li>
+
+		<li><a href="./espacePro.php">Espace pros. </a></li>
 
 	</ul>
-</li>
-
-<li> <a href="./sons.php">Sons</a></li>
-
-<li> <a href="./agenda.php">Agenda.</a></li>
-
-<li><a href="./lesMusicos.php">Les musiciens.</a></li>
-
-<li><a href="./formulaireRenseignement.php">Formulaire contact.</a></li>
-
-<li><a href="./espacePro.php">Espace pros. </a></li>
-
-</ul>
 </nav>
+
+
+
+
+
+
+
+</div>
