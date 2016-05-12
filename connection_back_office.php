29fcb330c3954b261004a4291c34a78e1338193a
domduf <minique.duf@gmail.com>
2016-05-12 07:52:32 +0200

    creation brouillon page connection bac-office

---
+++ connection_back_office.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<!-- ======================================================= -->
+<head>
+
+<meta charset="UTF-8" lang="fr"/>
+<meta name="Soul Latitude"  content="Groupe musique RythmNBlues dix musiciens concerts live "/>
+
+<link rel="icon" href="soullat2.ico" />
+
+<title>Soul Latitude | connection </title>
+<link href="./css/soul.css" rel="stylesheet" type="text/css" />
+</head>
+<!-- ======================================================= -->
+
+
+<body>
+
+<!-- ===================== TITRE ===================== -->
+<header id="TitreSoul">
+  <section>
+    <p>                </p>
+  </section>
+  <section id="Titre" >
+  
+    <H1>Connection </br>back-office <span class="cachee">Soul Latitude</span></H1>
+  </section>
+
+<!-- ===================== CONTACTS ===================== -->
+  <section id="contactsTitre">
+    <H2>Contacts</H2>
+    <p><a href="https://fr-fr.facebook.com/Soul-Latitude-330890707038975/"><img src="./images/facebook.ico" width="20%"/></a> Soul Latitude</p>
+    <p>Mobile: 06 72 27 66 00</p>
+  </section>
+</header>	
+
+<!-- ===================== VISUEL ===================== -->
+
+<section id="centre">
+<!-- ===================== MENU ===================== -->
+	<?php include("includes/menu.php"); ?>
+
+  	<span>
+  		<H2>Notes de version</H2>
+  		
+  		<p>Wed Apr 6	installation versionning by git</p>
+  		<p>Wed Apr 27	début de mise en place de la maj via ssh et Git sur hebergeur</br>
+  		voir <a href="./docs/publierAvecGitAutomatisation.png">ici la copie d'écran.<a/>
+  	</span> </p>
+  		<p>Sun May 1	simplification bd -> 1er mai</p>
+
+
+
+
+</section>
+
+
+
+<!-- ===================== BAS DE PAGE  ===================== -->
+<?php include("includes/basDePage.php"); ?>
+</body>
+
+</html>
