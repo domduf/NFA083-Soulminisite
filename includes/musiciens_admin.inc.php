c98a77800809e8efc049649cd15adc5c33524a4e
domduf <minique.duf@gmail.com>
2016-05-19 23:27:16 +0200

    modif include des admin musicos

--- includes/musiciens_admin.inc.php
+++ includes/musiciens_admin.inc.php
@@ -6,23 +6,29 @@
 <?php
 
 	$sqlmembre = "SELECT 	mem_id, mem_description_musico, mem_civilite,
-							mem_nom, mem_prenom, mem_date_naiss
+							mem_nom, mem_prenom, mem_date_naiss, mem_persona,
+							mem_activite
 					FROM 		membre " ;
 		
 		$membres= $pdo->query($sqlmembre); ?>
 		
 		<table id="tableau_messages">
-		<tr>
-			<th>id</th>
-			<th>Prénom Nom</th>
-			<th>description musico</th>
-		</tr>
+			<tr>
+				<th>id</th>
+				<th>zico ?</th>
+				<th>Prénom Nom</th>
+				<th>description musico</th>
+			</tr>
+
+
+
 		<?php 
 		while ($membre = $membres->fetch()) {
-		?> <tr>
-			<td><?php echo $membre['mem_id']?></td>
-			<td><?php echo $membre['mem_prenom'].' '.$membre['mem_nom']?></td>
-			<td><?php echo $membre['mem_description_musico']?></td>
+		?> 	<tr>
+				<td><?php echo $membre['mem_id']; if ($membre['mem_persona']=='Gestionaire'){echo '*';}?></td>
+				<td><?php if ($membre['mem_activite']=='oui'){ echo ' X ';}?></td>
+				<td><?php echo $membre['mem_prenom'].' '.$membre['mem_nom']?></td>
+				<td><?php echo $membre['mem_description_musico']?></td>
 			</tr>
 		<?php 
 		}?> 
