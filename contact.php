33896451f2cba87d196abb63e4f1aefa944b0775
domduf <minique.duf@gmail.com>
2016-05-20 17:53:00 +0200

    modif page contact, selection nom<=50 caracteres

--- contact.php
+++ contact.php
@@ -42,6 +42,8 @@
 <section id="accrocheAccueuil">
     <H3>Merci d'avoir rempli le formulaire.</H3>
     <p>Nous vous contactons au plus vite. </p>
+
+
 </section>
 <!-- ===================== VISUEL ===================== -->
 
@@ -53,10 +55,13 @@
 	<div>
 
 	<?php 	$nom=htmlentities($_POST['nom']);
-		echo ($nom. strlen($nom));
-		 if(strlen($nom)<=50) {
-								echo('OK');
-								}
+		
+
+	if(strlen($nom)<=50) 
+		
+		{ /* test de la longueur du nom <=50 caractères */
+
+		echo('OK pour le Nom');
 		$prenom=htmlentities($_POST['prenom']);
 		echo ($prenom);
 		$monMail=htmlentities($_POST['monMail']);
@@ -68,24 +73,34 @@
 		echo ($choix);
 		$message=htmlentities($_POST['rem']);
 		echo ($message);
-		
-		
 		?>
 		
 	<!-- ================ Connection bdd via PDO ================ -->
-<?php include("includes/connection.php"); ?>	
+	<?php include("includes/connection.php"); ?>	
 
 
-		<?php /* requete d'insertion préparée du message dans la table */
+		<?php 
+		
+		/* requète d'insertion préparée du message dans la bd */
 		$sqlInsertionMessage= 
 		"INSERT INTO contact (contact_nom, contact_prenom, contact_email, contact_telephone, contact_objet, contact_message)
 				VALUES (?,?,?,?,?,?)";
+
+
 		/* insertion via pdo */	
 		$stmt=$pdo->prepare($sqlInsertionMessage);
 		$nouveau_message=array($nom,$prenom,$monMail,$monTel,$choix,$message);
 		$stmt->execute($nouveau_message);
-		
-		
+		}
+
+		else {?>
+		<p>
+		<?php echo('Le champ \'Nom\' contient beaucoup trop de caractères...('.strlen($nom).') ne seriez-vous pas en train de fomenter quelque chose...');?>
+		</p>
+		<p><a href="formulaireRenseignement.php"><img src="./images/boutonRetour.png"></a> au formulaire de contact.</p>
+
+		<?php
+		}
 		?>
 
 	</div>
