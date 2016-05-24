a15fb7236afff7e739ea2d5f7696a12e70c53486
domduf <minique.duf@gmail.com>
2016-05-24 20:54:32 +0200

    modif basDePage et creation mentionsLegalesMembresEquipe.php

--- includes/basDePage.php
+++ includes/basDePage.php
@@ -2,8 +2,7 @@
 <!--Une ligne pour separer le bas de page
 <hr/>-->
 <p>site du groupe Soul Latitude.</p>
-<a href="./index.php">Accueil</a><br/>
-<a href="./formulaireRenseignement.php" >Contacts</a><br/>
+
 <a href="./planDuSite.php">Plan du site et documents utiles.</a><br/>
 <a href="./mentionsLegalesMembresEquipe.php">Mentions légales. </a><br/>
 <a href="./notesVersion.php">Notes de versions et derniers changements </a><br/>
---
+++ mentionsLegalesMembresEquipe.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<!-- ======================================================= -->
+<head>
+
+<meta charset="UTF-8" lang="fr"/>
+<meta name="Soul Latitude"  content="Groupe musique RythmNBlues dix musiciens concerts live "/>
+
+<link rel="icon" href="soullat2.ico" />
+
+<title>Soul Latitude | Mentions légales </title>
+<link href="./css/soul.css" rel="stylesheet" type="text/css" />
+</head>
+<!-- ======================================================= -->
+
+
+<body>
+
+
+
+<!-- ===================== TITRE ===================== -->
+<header id="TitreSoul">
+  <section>
+	<?php include("./includes/bouton_connect.inc.php"); ?>
+  </section>
+  <section id="Titre" >
+  
+    <H1>Mentions Légales <span class="cachee">Soul Latitude</span></H1>
+    
+  </section>
+
+<!-- ===================== CONTACTS ===================== -->
+  <section id="contactsTitre">
+    <H2>Contacts</H2>
+    <p><a href="https://fr-fr.facebook.com/Soul-Latitude-330890707038975/"><img src="./images/facebook.ico" width="20%"/></a> Soul Latitude</p>
+    <p>Mobile: 06 72 27 66 00</p>
+  </section>
+</header>
+<!-- ======================================================= -->
+<!-- ======================================================= -->
+
+
+<!-- ===================== VISUEL ===================== -->
+
+<section id="centre">
+<!-- ===================== MENU ===================== -->
+<?php include("includes/menu.php"); ?>
+
+<div>
+
+
+<H3>Hery Ratsimbasafy</H3>
+<p>Conception menu, PHP...</p>
+<H3>Dominque Dufour</H3>
+<p>Conception back-office, web-graphisme, Javascript formulaire, base de données... </p>
+<H3>Sandrine</H3>
+<p>dite "Dine"</p>
+<p>Photos, Conception graphique de la plaquette, dont s'est inspiré Dominique</p>
+</div>
+
+
+
+</section>
+<!-- ======================================================= -->
+<!-- ======================================================= -->
+
+<!-- ===================== BAS DE PAGE  ===================== -->
+<?php include("includes/basDePage.php"); ?>
+</body>
+
+</html>
