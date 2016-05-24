d523c40a36804426c08767807801c01529d0023d
domduf <minique.duf@gmail.com>
2016-05-24 18:10:47 +0200

    mise en forme et insertion de la dateTime en bd

--- contact.php
+++ contact.php
@@ -73,19 +73,46 @@
 		$choix=htmlentities($_POST['CHOIX']);
 		$message=htmlentities($_POST['rem']);	
 		
-		echo($nom);
+		/*recupération de la date et de l'heure*/
+		$contact_dateTime = date('Y-m-d H:i:s');?>
+	
+	<!-- ========================= Affichage comptre rendu du message ===================== -->
+		<H2>Merci M/Mme <?php echo($prenom.' '.$nom);?></H2>
+		
+		<p>Date et heure d'enregistrement de votre message </br>
+		<?php echo ($contact_dateTime); ?></p>
 
-		echo ($prenom);
+		<H2>Vos coordonnées: </H2>
 		
-		echo ($monMail);
-
+		<p>
+		<table id="tableau_messages">
+			<tr>
+				<td>Votre mail</td>
+				<td>Votre télephone</td>
+			</tr>
+			<tr>
+				<td><?php echo ($monMail);?></td>
+				<td><?php echo ($monTel);?></td>
+			</tr>
+		</table>
+		</p>
 		
-		echo ($monTel);
+		<H2>Votre missive: </H2>
+		<p>
+		<table id="tableau_messages">
+			<tr>
+				<td>Objet du message</td>
+				<td>Votre message</td>
+			</tr>
+			<tr>
+				<td><?php echo ($choix);?></td>
+				<td><?php echo ($message);?></td>
+			</tr>
+		</table>
+		</p>		
 		
-		echo ($choix);
 		
-		echo ($message);
-		?>
+		
 		
 		<!-- ================ Connection bdd via PDO ================ -->
 		<?php include("includes/connection.php"); ?>	
@@ -95,25 +122,21 @@
 		
 		/* requète d'insertion préparée du message dans la bd */
 		$sqlInsertionMessage= 
-		"INSERT INTO contact (contact_nom, contact_prenom, contact_email, contact_telephone, contact_objet, contact_message)
-				VALUES (?,?,?,?,?,?)";
+		"INSERT INTO contact (contact_nom, contact_prenom, contact_email, contact_telephone, contact_objet, contact_message, contact_dateTime)
+				VALUES (?,?,?,?,?,?,?)";
 
 
 		/* insertion via pdo */	
 		$stmt=$pdo->prepare($sqlInsertionMessage);
-		$nouveau_message=array($nom,$prenom,$monMail,$monTel,$choix,$message);
+		$nouveau_message=array($nom,$prenom,$monMail,$monTel,$choix,$message,$contact_dateTime);
 		$stmt->execute($nouveau_message);
 
-		/* affichage du message de l'heure d'envoi */
+		/* affichage du message de l'heure d'enregistrement */
 		?>
 		<p>
 		<?php 
-		$date_enregistrement_message = date('d/m/Y');
-		$heure_enregistrement_message = date('H/i/s');
-		echo ('Votre message a été enregistré en base de donnée le '.date('d/m/Y').' à '.date('H').'h'.date('i').'min'.date('s').'</br>');
-		echo ($date_enregistrement_message.' '.$heure_enregistrement_message);
+		echo ('Votre message a été enregistré en base de donnée le '.date('d/m/Y').' à '.date('H').'h'.date('i').'min et '.date('s').' s</br>');		
 		?>
-		
 		</p>
 		<?php 
 		}
